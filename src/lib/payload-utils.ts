1e447562bbdd03bcfd306da80859fd7bc29cee60
mark <mark>
2023-12-05 01:22:52 +0800

    added changes

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,11 +1,11 @@
-import { User } from '../payload-types'
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
-import { NextRequest } from 'next/server'
+import { User } from "@/payload-types";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { NextRequest } from "next/server";
 
 export const getServerSideUser = async (
-  cookies: NextRequest['cookies'] | ReadonlyRequestCookies
+  cookies: NextRequest["cookies"] | ReadonlyRequestCookies,
 ) => {
-  const token = cookies.get('payload-token')?.value
+  const token = cookies.get("payload-token")?.value;
 
   const meRes = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
@@ -13,12 +13,12 @@ export const getServerSideUser = async (
       headers: {
         Authorization: `JWT ${token}`,
       },
-    }
-  )
+    },
+  );
 
   const { user } = (await meRes.json()) as {
-    user: User | null
-  }
+    user: User | null;
+  };
 
-  return { user }
-}
+  return { user };
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,9 @@ import { getServerSideUser } from "./lib/payload-utils";
 
 export async function middleware(req: NextRequest) {
   const { nextUrl, cookies } = req;
-  const { user } = await getServerSideUser(cookies);
+  // const { user } = await getServerSideUser(cookies);
+
+  const user = null;
 
   if (user && ["/sign-in", "/sign-up"].includes(nextUrl.pathname)) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/`);
