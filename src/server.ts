a568d140eea542a1d4f954588cfce9da852a033f
mark <mark>
2023-12-05 02:19:01 +0800

    added server , nodemon setup

--- src/server.ts
+++ src/server.ts
@@ -81,7 +81,7 @@ const start = async () => {
     trpcExpress.createExpressMiddleware({
       router: appRouter,
       createContext,
-    }),
+    })
   );
 
   app.use((req, res) => nextHandler(req, res));
@@ -91,7 +91,7 @@ const start = async () => {
 
     app.listen(PORT, async () => {
       payload.logger.info(
-        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`,
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
       );
     });
   });
